54199956644a9598e26b1c5a37ba6caf48e504ee
Pablo Biasotti <pablo.biasotti@southworks.com>
2025-05-29 20:46:04 +0300

    force build error

--- application/src/app/(protected)/admin/dashboard/page.tsx
+++ application/src/app/(protected)/admin/dashboard/page.tsx
@@ -2,6 +2,6 @@ import AdminDashboard from '../../../../components/admin/AdminDashboard';
 
 export default async function DashboardPage() {
   return (
-    <AdminDashboard />
+    <AdminDashboard r />
   );
 }
