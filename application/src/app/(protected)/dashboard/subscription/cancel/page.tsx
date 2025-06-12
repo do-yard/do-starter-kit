912feee563a0f13f2d50c2ef4f6d235a4beb81ac
farg-sw <facundo.arguelles@southworks.com>
2025-06-12 14:36:34 -0300

    feat: [stripe] cleanup (#72)
    * remove unnecesary things
    
    * update stripe setup docs to use npm script
    
    * remove redundant doc
    
    ---------
    
    Co-authored-by: Lorenzo Fay <lorenzofay@gmail.com>

--- application/src/app/(protected)/dashboard/subscription/cancel/page.tsx
+++
@@ -1,9 +0,0 @@
-import Cancel from 'components/dashboard/subscription/Cancel';
-
-/**
- * Subscription cancellation page
- * @returns Page with subscription cancellation UI
- */
-export default function CancelPage() {
-  return <Cancel />;
-}
--- application/src/app/(protected)/dashboard/subscription/success/page.tsx
+++
@@ -1,9 +0,0 @@
-import Success from 'components/dashboard/subscription/Success';
-
-/**
- * Subscription success page
- * @returns Page with subscription success UI
- */
-export default function SuccessPage() {
-  return <Success />;
-}
